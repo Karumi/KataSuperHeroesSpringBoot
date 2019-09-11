26860c5d2b5938fc4ccf70e40c8e23344b0f9b6a
Jorge Juan Barroso Carmona <jorge@karumi.com>
2019-09-11 15:42:03 +0200

    first commit

---
+++ src/main/kotlin/com/karumi/superhero/SuperheroApplication.kt
@@ -0,0 +1,11 @@
+package com.karumi.superhero
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SuperheroApplication
+
+fun main(args: Array<String>) {
+	runApplication<SuperheroApplication>(*args)
+}
---
+++ src/test/kotlin/com/karumi/superhero/SuperheroApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.karumi.superhero
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SuperheroApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
