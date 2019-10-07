7eb5d304252899c96237aa52b8dd7cac475c39fd
Davide Mendolia <davide@gokarumi.com>
2019-10-07 16:16:58 +0200

    Update Kotlin Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   id("org.springframework.boot") version "2.1.8.RELEASE"
   id("io.spring.dependency-management") version "1.0.8.RELEASE"
-  kotlin("jvm") version "1.2.71"
-  kotlin("plugin.spring") version "1.2.71"
+  kotlin("jvm") version "1.3.50"
+  kotlin("plugin.spring") version "1.3.50"
 }
 
 group = "com.karumi"
